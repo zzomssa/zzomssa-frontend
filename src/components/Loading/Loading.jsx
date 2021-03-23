0b89d76053eab867092612b4e46d072b777d0352
jinsoo <admin@binary01.me>
2021-03-23 17:35:17 +0900

    feat : loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import tw from 'twin.macro';
+
+import LoadingTools from '../../constants/loadingItem';
+
+const LoadingBackground = styled.div(
+  tw`bg-black bg-opacity-50 outline-none`,
+);
+
+const LoadingIcon = styled.img(
+  tw`w-10 h-10 z-100`,
+  css`
+    transform-origin: 50% 50%;
+    animation: spin 1.5s cubic-bezier(0.8, 0, 0.2, 1) infinite;
+    @keyframes spin {
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `,
+);
+
+const Loading = () => (
+  <LoadingBackground>
+    <LoadingIcon src={LoadingTools.Logo} />
+  </LoadingBackground>
+);
+
+export default Loading;
---
+++ src/constants/loadingItem.js
@@ -0,0 +1,7 @@
+import Logo from '../assets/icons/logo_rotate.svg';
+
+const LoadingTools = {
+  Logo,
+};
+
+export default LoadingTools;
