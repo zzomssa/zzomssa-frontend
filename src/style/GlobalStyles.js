45ece35239374eca06d2d0efbc76e24a35db85f3
jinsoo <admin@binary01.me>
2021-03-25 18:33:06 +0900

    fix: index html delete globalstyle import add

--- src/style/GlobalStyles.js
+++ src/style/GlobalStyles.js
@@ -3,12 +3,13 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
+  @import url(//fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap);
   body {
     margin : 0;
     background: ${({ theme }) => theme.background} !important;
     color: ${({ theme }) => theme.text} !important;
     border-color: ${({ theme }) => theme.vertical_border} !important;
-    font-family: 'Lato', 'Noto Sans KR', sans-serif;
+    font-family: 'Lato', sans-serif;
     transition: all 0.25s linear;
     -webkit-tap-highlight-color : transparent;
   }
