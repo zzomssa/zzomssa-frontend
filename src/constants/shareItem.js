2798ae16589ca3b58b237e92f355aa623238ae02
jinsoo <admin@binary01.me>
2021-04-09 15:20:41 +0900

    fix : www delete

--- src/constants/shareItem.js
+++ src/constants/shareItem.js
@@ -1,6 +1,6 @@
 import kakaoShareLogo from '../assets/icons/kakao_logo.png';
 
-const ZZOMSSA_URL = `https://www.zzomssa.com`;
+const ZZOMSSA_URL = `https://zzomssa.com`;
 const ZZOMSSA_NAME = `zzomssa`;
 
 export { kakaoShareLogo, ZZOMSSA_URL, ZZOMSSA_NAME };
